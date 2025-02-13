3762b256d1d9b12610a673e403d8d9184aa0a2a3
Bellika <markusbellika@gmail.com>
2025-02-13 10:02:54 +0100

    connection do mongodb working

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const cors = require('cors')
+const connectDB = require('./config/db')
+require('dotenv').config()
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5001
+
+connectDB()
 
 app.use(cors())
+app.use(express.json())
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: "Hello"})
